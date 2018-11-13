cbfaeac78fbe76cc2ddf341b594151b5256c7409
bob-williams-1 <bob.williams.dev@gmail.com>
2018-11-13 11:45:57 -0500

    commit 4

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,10 @@
 // array of default friends
 
 var friends = [{
-  name: "John Torpey",
-  photo: "https://static.pexels.com/photos/343717/pexels-photo-343717.jpeg",
+  name: "Marc Levetin",
+  photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjTm-qe6NHeAhUIh-AKHd4jDM4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fmarclevetin&psig=AOvVaw2h-J76N5V-nSaFmcp0WTEZ&ust=1542213795111866",
   scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
-}, {
-  name: "Laura Toner",
-  photo: "https://static.pexels.com/photos/206481/pexels-photo-206481.jpeg",
-  scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
-}, {
-  name: "Bob Merad",
-  photo: "https://static.pexels.com/photos/249760/pexels-photo-249760.jpeg",
-  scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
-},{
-  name: "Amy Crosby",
-  photo: "https://static.pexels.com/photos/270308/pexels-photo-270308.jpeg",
-  scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
-}, {
-  name: "Chris Maclin",
-  photo: "https://static.pexels.com/photos/249757/pexels-photo-249757.jpeg",
-  scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
-}, {
-  name: "Anna Schultz",
-  photo: "https://static.pexels.com/photos/388517/pexels-photo-388517.jpeg",
-  scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
-}, {
-  name: "Jack Waller",
-  photo: "https://static.pexels.com/photos/64385/pexels-photo-64385.jpeg",
-  scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+
 }];
 
  //export array
